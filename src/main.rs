fab672ef45990ee46ef5f8f46682a92b85d7db83
rbreckenridge <rbreckenridge@teradici.com>
2022-02-04 13:31:13 -0800

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// &'static is a "lifetime specifier", something you'll learn more about later
+pub fn hello() -> &'static str {
+    "Hello, World!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    hello();
+}
+
+#[test]
+fn test_hello_world() {
+    assert_eq!("Hello, World!", crate::hello());
 }
